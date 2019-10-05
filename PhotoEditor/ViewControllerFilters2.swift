bca165b39dd0f63fe4ba716a6e15c1e8556d6ddd
kosyaKosia <dasha-kosyakova@mail.ru>
2019-10-05 11:52:33 +0300

    Try to fix saving image

--- PhotoEditor/ViewControllerFilters2.swift
+++ PhotoEditor/ViewControllerFilters2.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ViewControllerFilters2: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
@@ -17,5 +18,50 @@ class ViewControllerFilters2: UIViewController {
         super.viewDidLoad()
 
         imageView.image = filteredImage!
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let image = imageView.image!
+        
+        PHPhotoLibrary.requestAuthorization({ status in
+            guard status == .authorized else {
+                return
+            }
+            PHPhotoLibrary.shared().performChanges({
+                let changeRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+                let objectPlaceholder = changeRequest.placeholderForCreatedAsset
+                let albumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: "Test")
+                let enumeration: NSArray = [objectPlaceholder!]
+                albumRequest.addAssets(enumeration)
+            }, completionHandler: { isSaved, error in
+                if let error = error {
+                    NSLog("🛑 An error appeared while saving photo: " + error.localizedDescription)
+                    return
+                }
+                if isSaved {
+                    NSLog("✅ Successfully saved photo to library.")
+                } else {
+                    NSLog("wrong")
+                }
+                
+            })
+        })
+        
+//        UIImageWriteToSavedPhotosAlbum(
+//            imageView.image!,
+//            self,
+//            #selector(image(_:didFinishSavingWithError:contextInfo:)), nil
+//        )
+    }
+    
+    @objc private func image(_ image: UIImage,
+                             didFinishSavingWithError error: Error?,
+                             contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            NSLog("🛑 An error appeared while saving photo: " + error.localizedDescription)
+            return
+        }
+        NSLog("✅ Successfully saved photo to library.")
     }
 }
